6354168a0ec94a999093629538b50f3a77b36273
Alex <alexander.loew@lmu.de>
2017-06-27 10:09:02 +0200

    directory is returned for sar data access component

--- multiply_data_access/sar_data_access/dummy_sar_data_access_provider.py
+++ multiply_data_access/sar_data_access/dummy_sar_data_access_provider.py
@@ -1,8 +1,11 @@
 # import snappy
 
+import tempfile
+
 class DummySARDataAccessProvider:
 
     def get_data(self, temporal_location, spatial_location, constraints):
         print ('Retrieving SAR data')
         # return snappy.Product('sar_data', 'sar_data', 1, 1)
-        return 'sar_product'
+        # returns a directory
+        return tempfile.mkdtemp()
