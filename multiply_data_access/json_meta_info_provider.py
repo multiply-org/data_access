266ed859c569b1f725c685750a63b7334c6edb55
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-02-04 00:24:11 +0100

    removed prints

--- multiply_data_access/json_meta_info_provider.py
+++ multiply_data_access/json_meta_info_provider.py
@@ -23,23 +23,18 @@ class JsonMetaInfoProvider(MetaInfoProvider):
         query_end_time = self.get_end_time_from_query_string(query_string)
         data_types = self.get_data_types_from_query_string(query_string)
         data_set_meta_infos = []
-        print('start examining data sets')
-        print(self.data_set_infos['data_sets'])
         for data_set_info in self.data_set_infos['data_sets']:
             if data_set_info.get('coverage') is not None and roi is not None:
                 data_set_coverage = loads(data_set_info.get('coverage'))
                 if not roi.intersects(data_set_coverage):
-                    print('roi does not intersect coverage')
                     continue
             if data_set_info.get('start_time') is not None:
                 data_set_start_time = DataUtils.get_time_from_string(data_set_info.get('start_time'), False)
                 if query_end_time < data_set_start_time:
-                    print('query end time is before data set start time')
                     continue
             if data_set_info.get('end_time') is not None:
                 data_set_end_time = DataUtils.get_time_from_string(data_set_info.get('end_time'), True)
                 if data_set_end_time < query_start_time:
-                    print('query start time is after data set end time')
                     continue
             if data_set_info.get('data_type') in data_types:
                 data_set_meta_info = DataSetMetaInfo(coverage=data_set_info.get('coverage'),
