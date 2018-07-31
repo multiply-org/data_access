942fad22d97d194f3782001b68329704fb797410
TonioF <tonio.fincke@brockmann-consult.de>
2018-07-31 14:01:10 +0200

    added entries for lpdaac

--- setup.py
+++ setup.py
@@ -24,11 +24,14 @@ setup(name='multiply-data-access',
           'file_system_plugins': [
               'local_file_system = multiply_data_access:local_file_system.LocalFileSystemAccessor',
               'aws_s2_file_system = multiply_data_access:aws_s2_file_system.AwsS2FileSystemAccessor',
+              'lpdaac_file_system = multiply_data_access:lpdaac_data_access.LpDaacAccessFileSystemAccessor',
           ],
           'meta_info_provider_plugins': [
               'json_meta_info_provider = multiply_data_access:json_meta_info_provider.JsonMetaInfoProviderAccessor',
               'aws_s2_meta_info_provider = multiply_data_access:aws_s2_meta_info_provider.AwsS2MetaInfoProviderAccessor',
+              'lpdaac_meta_info_provider = '
+              'multiply_data_access:lpdaac_meta_info_provider.LpDaacMetaInfoProviderAccessor',
           ],
       },
       install_requires=requirements
-)
+      )
