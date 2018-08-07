15321f779936acdf231f1a40c3c2c2b70c110b72
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-08-07 14:21:56 +0200

    introduced vrt file system

--- multiply_data_access/vrt_data_access.py
+++ multiply_data_access/vrt_data_access.py
@@ -8,21 +8,20 @@ format that is globally an.
 
 import gdal
 import os
+import xml.etree.ElementTree as ET
 
-from shapely.geometry import MultiPolygon, Polygon
+from shapely.geometry import Polygon
 from shapely.ops import cascaded_union
 from shapely.wkt import loads
-from typing import List, Optional, Sequence
-import urllib.request as urllib2
+from typing import List, Sequence
 
-from multiply_core.observations import get_file_pattern, is_valid_for
 from multiply_core.util import FileRef, get_mime_type
-from multiply_data_access.data_access import DataSetMetaInfo, FileSystemAccessor, MetaInfoProvider, \
+from multiply_data_access.data_access import DataSetMetaInfo, FileSystem, FileSystemAccessor, MetaInfoProvider, \
     MetaInfoProviderAccessor
 from multiply_data_access.data_access_component import DataAccessComponent
-from multiply_data_access.data_set_meta_info_extraction import DataSetMetaInfoProvision
 
 _META_INFO_PROVIDER_NAME = 'VrtMetaInfoProvider'
+_FILE_SYSTEM_NAME = 'VrtFileSystem'
 
 
 class VrtMetaInfoProvider(MetaInfoProvider):
@@ -48,29 +47,49 @@ class VrtMetaInfoProvider(MetaInfoProvider):
     def query(self, query_string: str) -> List[DataSetMetaInfo]:
         roi = self.get_roi_from_query_string(query_string)
         if not os.path.exists(self._path_to_vrt_file):
-            coverages = self._get_coverages_from_wrapped_meta_info_provider(query_string)
+            coverages, referenced_data = self._get_coverages_from_wrapped_meta_info_provider(query_string)
             coverage = cascaded_union(coverages)
         else:
             vrt_data_set = gdal.Open(self._path_to_vrt_file)
             vrt_coverage = loads(vrt_data_set.GetMetadataItem('COVERAGE'))
+            referenced_data = self._get_referenced_data_sets_from_vrt()
             if roi.within(vrt_coverage):
                 coverage = vrt_coverage
             else:
-                coverages = self._get_coverages_from_wrapped_meta_info_provider(query_string)
+                coverages, additional_files = self._get_coverages_from_wrapped_meta_info_provider(query_string)
+                for additional_file in additional_files:
+                    if additional_file not in referenced_data:
+                        referenced_data.append(additional_file)
                 coverages.append(vrt_coverage)
                 coverage = cascaded_union(coverages)
-        data_set_meta_info = DataSetMetaInfo(coverage.wkt, None, None, 'VRT', self._path_to_vrt_file)
+        if referenced_data is not None:
+            referenced_data = ';'.join(referenced_data)
+        data_set_meta_info = DataSetMetaInfo(coverage.wkt, None, None, 'VRT', self._path_to_vrt_file, referenced_data)
         return [data_set_meta_info]
 
-    def _get_coverages_from_wrapped_meta_info_provider(self, query_string:str) -> List[Polygon]:
+    def _get_referenced_data_sets_from_vrt(self) -> List[str]:
+        referenced_data_sets = []
+        if not os.path.exists(self._path_to_vrt_file):
+            return referenced_data_sets
+        tree = ET.parse(self._path_to_vrt_file)
+        root = tree.getroot()
+        raster_bands = root.findall('VRTRasterBand')
+        for raster_band in raster_bands:
+            referenced_data_set = raster_band.find('SimpleSource').find('SourceFilename').text
+            referenced_data_sets.append(referenced_data_set.split('/')[-1])
+        return referenced_data_sets
+
+    def _get_coverages_from_wrapped_meta_info_provider(self, query_string:str) -> (List[Polygon], List[str]):
         split_string = query_string.split(';')
         split_string[-1] = self._encapsulated_data_type
         new_query_string = ';'.join(split_string)
         encapsulated_data_meta_set_infos = self._wrapped_meta_info_provider.query(new_query_string)
         coverages = []
+        names = []
         for data_set_meta_info in encapsulated_data_meta_set_infos:
             coverages.append(loads(data_set_meta_info.coverage))
-        return coverages
+            names.append(data_set_meta_info.identifier)
+        return coverages, names
 
     def provides_data_type(self, data_type: str) -> bool:
         return data_type == 'VRT'
@@ -82,6 +101,7 @@ class VrtMetaInfoProvider(MetaInfoProvider):
         parameters.update(self._wrapped_meta_info_provider.get_as_dict()['parameters'])
         return parameters
 
+
 class VrtMetaInfoProviderAccessor(MetaInfoProviderAccessor):
 
     @classmethod
@@ -91,3 +111,74 @@ class VrtMetaInfoProviderAccessor(MetaInfoProviderAccessor):
     @classmethod
     def create_from_parameters(cls, parameters: dict) -> VrtMetaInfoProvider:
         return VrtMetaInfoProvider(parameters)
+
+
+class VrtFileSystem(FileSystem):
+
+    def __init__(self, parameters: dict):
+        if 'path_to_vrt_file' not in parameters:
+            raise ValueError('No vrt file provided')
+        self._path_to_vrt_file = parameters['path_to_vrt_file']
+        if 'encapsulated_data_type' not in parameters:
+            raise ValueError('Vrt meta info provider must know encapsulated data type')
+        self._encapsulated_data_type = parameters['encapsulated_data_type']
+        data_access_component = DataAccessComponent()
+        file_system_as_dict = {'type': parameters['accessed_file_system'], 'parameters': parameters}
+        self._file_system = data_access_component.create_file_system_from_dict(file_system_as_dict)
+
+    @classmethod
+    def name(cls) -> str:
+        return _FILE_SYSTEM_NAME
+
+    def get(self, data_set_meta_info: DataSetMetaInfo) -> Sequence[FileRef]:
+        if data_set_meta_info.referenced_data is None:
+            return []
+        required_datasets = []
+        referenced_data_sets = data_set_meta_info.referenced_data.split(';')
+        for data_set in referenced_data_sets:
+            # coverage is wrong here. We leave it as it makes no difference.
+            file_refs = self._file_system.get(DataSetMetaInfo(data_set_meta_info.coverage, None, None,
+                                                              self._encapsulated_data_type, data_set))
+            for file_ref in file_refs:
+                if file_ref.url not in required_datasets:
+                    required_datasets.append(file_ref.url)
+        vrt_dataset = gdal.BuildVRT(self._path_to_vrt_file, required_datasets)
+        vrt_dataset.SetMetadataItem('COVERAGE', data_set_meta_info.coverage)
+        vrt_dataset.FlushCache()
+        self._set_absolute_sources(required_datasets)
+        return [FileRef(self._path_to_vrt_file, None, None, get_mime_type(self._path_to_vrt_file))]
+
+    def _set_absolute_sources(self, file_refs: List[str]):
+        if not os.path.exists(self._path_to_vrt_file):
+            return
+        tree = ET.parse(self._path_to_vrt_file)
+        root = tree.getroot()
+        raster_bands = root.findall('VRTRasterBand')
+        for raster_band in raster_bands:
+            source_file_name_element = raster_band.find('SimpleSource').find('SourceFilename')
+            if 'relativeToVRT' in source_file_name_element.attrib and \
+                    source_file_name_element.attrib['relativeToVRT'] == '1':
+                file_name = source_file_name_element.text.split('/')[-1]
+                for file_ref in file_refs:
+                    if file_ref.split('/')[-1] == file_name:
+                        source_file_name_element.text = file_ref
+                        source_file_name_element.attrib['relativeToVRT'] = '0'
+        tree.write(self._path_to_vrt_file)
+
+    def get_parameters_as_dict(self) -> dict:
+        parameters = {'path_to_vrt_file': self._path_to_vrt_file,
+                      'encapsulated_data_type': self._encapsulated_data_type,
+                      'accessed_file_system': self._file_system.name()}
+        parameters.update(self._file_system.get_as_dict()['parameters'])
+        return parameters
+
+
+class VrtFileSystemAccessor(FileSystemAccessor):
+
+    @classmethod
+    def name(cls) -> str:
+        return _FILE_SYSTEM_NAME
+
+    @classmethod
+    def create_from_parameters(cls, parameters: dict) -> FileSystem:
+        return VrtFileSystem(parameters)
--- test/data_access/test_vrt_data_access.py
+++ test/data_access/test_vrt_data_access.py
@@ -4,7 +4,8 @@ from multiply_data_access.lpdaac_data_access import DataSetMetaInfo, LpDaacFileS
 import os
 
 from multiply_core.observations import DataTypeConstants
-from multiply_data_access.vrt_data_access import VrtMetaInfoProvider, VrtMetaInfoProviderAccessor
+from multiply_data_access.vrt_data_access import VrtMetaInfoProvider, VrtMetaInfoProviderAccessor, VrtFileSystem, \
+    VrtFileSystemAccessor
 from shapely.wkt import loads
 
 H17_V05_COVERAGE = 'POLYGON ((-13.05407289035348 39.99999999616804, -11.54700538146705 29.9999999970181, ' \
@@ -14,6 +15,8 @@ H17_V04_COVERAGE = 'POLYGON ((-15.55723826442343 49.99999999531797, -13.05407289
                    '1.343193041889809e-09 49.99999999531797, 1.127072786096139e-09 39.99999999616804, ' \
                    '-15.55723826442343 49.99999999531797))'
 PATH_TO_VRT_FILE = './test/test_data/global_dem.vrt'
+PATH_TO_OTHER_VRT_FILE = './test/test_data/global_dem_2.vrt'
+PATH_TO_NON_EXISTENT_VRT_FILE = './test/test_data/global_dem_3.vrt'
 PATH_TO_JSON_FILE = './test/test_data/aster_meta_info.json'
 ASTER_POLYGON = "POLYGON ((-5. 37., -4. 37., -4. 36., -5. 36., -5. 37.))"
 
@@ -51,6 +54,8 @@ def test_vrt_meta_info_provider_query_vrt_file_does_not_exist_yet():
     assert data_set_meta_infos[0].end_time is None
     assert 'VRT' == data_set_meta_infos[0].data_type
     assert 'a_non_existing_file' == data_set_meta_infos[0].identifier
+    assert data_set_meta_infos[0].referenced_data is not None
+    assert 'ASTGTM2_N36W005_dem.tif' == data_set_meta_infos[0].referenced_data
 
 
 def test_vrt_meta_info_provider_query_existing_vrt_file_can_use_existing_vrt():
@@ -69,6 +74,8 @@ def test_vrt_meta_info_provider_query_existing_vrt_file_can_use_existing_vrt():
     assert data_set_meta_infos[0].end_time is None
     assert 'VRT' == data_set_meta_infos[0].data_type
     assert PATH_TO_VRT_FILE == data_set_meta_infos[0].identifier
+    assert data_set_meta_infos[0].referenced_data is not None
+    assert 'ASTGTM2_N36W005_dem.tif' == data_set_meta_infos[0].referenced_data
 
 
 def test_vrt_meta_info_provider_query_existing_vrt_file_new_vrt():
@@ -87,6 +94,8 @@ def test_vrt_meta_info_provider_query_existing_vrt_file_new_vrt():
     assert data_set_meta_infos[0].end_time is None
     assert 'VRT' == data_set_meta_infos[0].data_type
     assert PATH_TO_VRT_FILE == data_set_meta_infos[0].identifier
+    assert data_set_meta_infos[0].referenced_data is not None
+    assert 'ASTGTM2_N36W005_dem.tif;ASTGTM2_N36W006_dem.tif' == data_set_meta_infos[0].referenced_data
 
 
 def test_vrt_meta_info_provider_query_existing_vrt_file_new_vrt_with_multipolygon():
@@ -105,6 +114,8 @@ def test_vrt_meta_info_provider_query_existing_vrt_file_new_vrt_with_multipolygo
     assert data_set_meta_infos[0].end_time is None
     assert 'VRT' == data_set_meta_infos[0].data_type
     assert PATH_TO_VRT_FILE == data_set_meta_infos[0].identifier
+    assert data_set_meta_infos[0].referenced_data is not None
+    assert 'ASTGTM2_N36W005_dem.tif;ASTGTM2_N38W007_dem.tif' == data_set_meta_infos[0].referenced_data
 
 
 def test_vrt_meta_info_provider_provides_data_type():
@@ -132,3 +143,73 @@ def test_vrt_meta_info_provider_get_parameters_as_dict():
     assert 'JsonMetaInfoProvider' == dict['accessed_meta_info_provider']
     assert 'path_to_json_file' in dict.keys()
     assert PATH_TO_JSON_FILE == dict['path_to_json_file']
+
+
+def test_vrt_meta_info_provider_get_referenced_data_sets_from_vrt():
+    parameters = {'path_to_vrt_file': PATH_TO_OTHER_VRT_FILE, 'encapsulated_data_type': DataTypeConstants.ASTER,
+                  'accessed_meta_info_provider': 'JsonMetaInfoProvider', 'path_to_json_file': PATH_TO_JSON_FILE}
+    provider = VrtMetaInfoProviderAccessor.create_from_parameters(parameters)
+
+    data_sets_from_vrt = provider._get_referenced_data_sets_from_vrt()
+
+    assert 1 == len(data_sets_from_vrt)
+    assert 'ASTGTM2_N36W005_dem.tif' == data_sets_from_vrt[0]
+
+
+def test_vrt_file_system_create():
+    parameters = {'path_to_vrt_file': PATH_TO_VRT_FILE, 'encapsulated_data_type': DataTypeConstants.ASTER,
+                  'accessed_file_system': 'LocalFileSystem', 'path': './test/test_data/', 'pattern': '/dt/'}
+    file_system = VrtFileSystemAccessor.create_from_parameters(parameters)
+
+    assert file_system is not None
+
+
+def test_vrt_file_system_name():
+    parameters = {'path_to_vrt_file': PATH_TO_VRT_FILE, 'encapsulated_data_type': DataTypeConstants.ASTER,
+                  'accessed_file_system': 'LocalFileSystem', 'path': './test/test_data/', 'pattern': '/dt/'}
+    file_system = VrtFileSystemAccessor.create_from_parameters(parameters)
+
+    assert 'VrtFileSystem' == file_system.name()
+    assert 'VrtFileSystem' == VrtFileSystem.name()
+    assert 'VrtFileSystem' == VrtFileSystemAccessor.name()
+
+
+def test_vrt_file_system_get_parameters_as_dict():
+    parameters = {'path_to_vrt_file': PATH_TO_VRT_FILE, 'encapsulated_data_type': DataTypeConstants.ASTER,
+                  'accessed_file_system': 'LocalFileSystem', 'path': './test/test_data/', 'pattern': '/dt/'}
+    file_system = VrtFileSystemAccessor.create_from_parameters(parameters)
+
+    dict = file_system.get_parameters_as_dict()
+
+    assert 5 == len(dict)
+    assert 'path_to_vrt_file' in dict.keys()
+    assert PATH_TO_VRT_FILE == dict['path_to_vrt_file']
+    assert 'encapsulated_data_type' in dict.keys()
+    assert 'ASTER' == dict['encapsulated_data_type']
+    assert 'accessed_file_system' in dict.keys()
+    assert 'LocalFileSystem' == dict['accessed_file_system']
+    assert 'path' in dict.keys()
+    assert './test/test_data/' == dict['path']
+    assert 'pattern' in dict.keys()
+    assert '/dt/' == dict['pattern']
+
+
+def test_vrt_file_system_get():
+    parameters = {'path_to_vrt_file': PATH_TO_NON_EXISTENT_VRT_FILE, 'encapsulated_data_type': DataTypeConstants.ASTER,
+                  'accessed_file_system': 'LocalFileSystem', 'path': './test/test_data/', 'pattern': '/dt/'}
+    file_system = VrtFileSystemAccessor.create_from_parameters(parameters)
+
+    try:
+        data_set_meta_info = DataSetMetaInfo('of no concern here', None, None, DataTypeConstants.ASTER,
+                                             PATH_TO_NON_EXISTENT_VRT_FILE, 'ASTGTM2_N36W005_dem.tif')
+        file_refs = file_system.get(data_set_meta_info)
+        assert 1 == len(file_refs)
+        assert PATH_TO_NON_EXISTENT_VRT_FILE == file_refs[0].url
+        assert None == file_refs[0].start_time
+        assert None == file_refs[0].end_time
+        assert 'x-world/x-vrt' == file_refs[0].mime_type
+
+        assert os.path.exists(PATH_TO_NON_EXISTENT_VRT_FILE)
+    finally:
+        if os.path.exists(PATH_TO_NON_EXISTENT_VRT_FILE):
+            os.remove(PATH_TO_NON_EXISTENT_VRT_FILE)
