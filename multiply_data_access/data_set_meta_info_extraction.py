68b8ad1fe458dc5685bec1e422585779d8d4bd8c
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-08-16 14:33:14 +0200

    added modis mcd43 meta data info extractor

--- multiply_data_access/data_set_meta_info_extraction.py
+++ multiply_data_access/data_set_meta_info_extraction.py
@@ -11,7 +11,7 @@ __author__ = 'Tonio Fincke (Brockmann Consult GmbH)'
 from abc import ABCMeta, abstractmethod
 from multiply_data_access.data_access import DataSetMetaInfo
 from multiply_core.observations import DataTypeConstants
-from multiply_core.util import reproject
+from multiply_core.util import reproject, get_time_from_year_and_day_of_year
 from shapely.geometry import Point, Polygon
 from typing import Optional
 import gdal
@@ -110,17 +110,64 @@ class AsterMetaInfoExtractor(DataSetMetaInfoExtractor):
         return DataTypeConstants.ASTER
 
     def extract_meta_info(self, path: str) -> DataSetMetaInfo:
-        path_lat_id = path[8:9]
-        path_lat = float(path[9:11])
+        path_lat_id = path[-14:-12]
+        path_lat = float(path[-14:-12])
         if path_lat_id == 'S':
             path_lat *= -1
-        path_lon_id = path[11:12]
-        path_lon = float(path[12:15])
+        path_lon_id = path[-12:-11]
+        path_lon = float(path[-11:-8])
         if path_lon_id == 'W':
             path_lon *= -1
         coverage = Polygon([[path_lon, path_lat], [path_lon, path_lat + 1], [path_lon + 1, path_lat + 1],
                             [path_lon + 1, path_lat]])
         return DataSetMetaInfo(coverage.wkt, None, None, DataTypeConstants.ASTER, path)
+
+
+class MODISMCD43MetaInfoExtractor(DataSetMetaInfoExtractor):
+
+    def __init__(self):
+        self._X_STEP = -463.31271653 * 2400
+        self._Y_STEP = 463.31271653 * 2400
+        self._M_Y0 = -20015109.354
+        self._M_X0 = 10007554.677
+        wgs84_srs = osr.SpatialReference()  # Define a SpatialReference object
+        wgs84_srs.ImportFromEPSG(4326)  # And set it to WGS84 using the EPSG code
+        modis_sinu_srs = osr.SpatialReference()  # define the SpatialReference object
+        modis_sinu_srs.ImportFromProj4(
+            "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs")
+        self._wgs84_to_modis = osr.CoordinateTransformation(wgs84_srs, modis_sinu_srs)
+        self._modis_to_wgs84 = osr.CoordinateTransformation(modis_sinu_srs, wgs84_srs)
+
+    @classmethod
+    def name(cls) -> str:
+        return DataTypeConstants.MODIS_MCD_43
+
+    def extract_meta_info(self, path: str) -> DataSetMetaInfo:
+        h = int(path[-27:-25])
+        v = int(path[-24:-22])
+        tile_coverage = self._get_tile_coverage(h, v).wkt
+        year = int(path[-17:-13])
+        doy = int(path[-13:-10])
+        date = get_time_from_year_and_day_of_year(year, doy)
+        return DataSetMetaInfo(tile_coverage, date.strftime('%Y-%m-%d'), date.strftime('%Y-%m-%d'),
+                               DataTypeConstants.MODIS_MCD_43, path[-37:])
+
+    def _get_tile_coverage(self, h: int, v: int) -> Polygon:
+        sinu_min_lat = h * self._Y_STEP + self._M_Y0
+        sinu_max_lat = (h + 1) * self._Y_STEP + self._M_Y0
+        sinu_min_lon = v * self._X_STEP + self._M_X0
+        sinu_max_lon = (v + 1) * self._X_STEP + self._M_X0
+        points = []
+        lat0, lon0, z0 = self._modis_to_wgs84.TransformPoint(sinu_min_lat, sinu_min_lon)
+        points.append(Point(lat0, lon0))
+        lat1, lon1, z1 = self._modis_to_wgs84.TransformPoint(sinu_min_lat, sinu_max_lon)
+        points.append(Point(lat1, lon1))
+        lat2, lon2, z2 = self._modis_to_wgs84.TransformPoint(sinu_max_lat, sinu_min_lon)
+        points.append(Point(lat2, lon2))
+        lat3, lon3, z3 = self._modis_to_wgs84.TransformPoint(sinu_max_lat, sinu_max_lon)
+        points.append(Point(lat3, lon3))
+        polygon = Polygon([[p.x, p.y] for p in points])
+        return polygon
 
 
 class S2aMetaInfoExtractor(DataSetMetaInfoExtractor):
@@ -173,11 +220,12 @@ class DataSetMetaInfoProvision(object):
         self.add_data_set_meta_info_provider(S2bMetaInfoExtractor())
         self.add_data_set_meta_info_provider(WvMetaInfoExtractor())
         self.add_data_set_meta_info_provider(CamsMetaInfoExtractor())
+        self.add_data_set_meta_info_provider(MODISMCD43MetaInfoExtractor())
 
     def add_data_set_meta_info_provider(self, data_set_meta_info_provider: DataSetMetaInfoExtractor):
         self.DATA_SET_META_INFO_PROVIDERS.append(data_set_meta_info_provider)
 
-    def get_data_set_meta_info(self, data_type: str, path:str) -> Optional[DataSetMetaInfo]:
+    def get_data_set_meta_info(self, data_type: str, path: str) -> Optional[DataSetMetaInfo]:
         for data_set_meta_info_provider in self.DATA_SET_META_INFO_PROVIDERS:
             if data_set_meta_info_provider.name() == data_type:
                 return data_set_meta_info_provider.extract_meta_info(path)
