6140205f16bbcd2a852856d36c0cfde656c4925b
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-08-17 10:23:57 +0200

    moved logging

--- multiply_data_access/aws_s2_file_system.py
+++ multiply_data_access/aws_s2_file_system.py
@@ -54,14 +54,14 @@ class AwsS2FileSystem(LocallyWrappedFileSystem):
         aws_index = self._get_aws_index(data_set_meta_info.identifier)
         request = AwsTileRequest(tile=tile_name, time=time, aws_index=aws_index,
                                  bands=bands, metafiles=metafiles, data_folder=self._temp_dir)
-        request.save_data()
         year = start_time_as_datetime.year
         month = start_time_as_datetime.month
         day = start_time_as_datetime.day
+        logging.info('Downloading S2 Data from {}-{}-{}'.format(month, day, year))
+        request.save_data()
         saved_dir = '{}/{},{}-{:02d}-{:02d},{}/'.format(self._temp_dir, tile_name, year, month, day, aws_index)
         new_dir = '{0}{1}/{2}/{3}/{4}/{5}/{6}/{7}/'.format(saved_dir, tile_name[0:2], tile_name[2:3], tile_name[3:5],
                                                            year, month, day, aws_index)
-        logging.info('Downloading S2 Data from {}-{}-{}'.format(month, day, year))
         copy_tree(saved_dir, new_dir)
         logging.info('Downloaded S2 Data from {}-{}-{}'.format(month, day, year))
         return FileRef(new_dir, data_set_meta_info.start_time, data_set_meta_info.end_time, get_mime_type(new_dir))
