6d6f0a4e9e9c6f81c4f9babd0d5d9544fb56283a
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-08-17 12:49:10 +0200

    removed data access example script to main folder

---
+++ multiply_data_access/data_access_test.py
@@ -0,0 +1,32 @@
+# test script that actually downloads S2 data. Execute with care
+
+from multiply_data_access import DataAccessComponent
+
+data_access_component = DataAccessComponent()
+data_access_component.get_provided_data_types()
+BARRAX_POLYGON = "POLYGON((-2.20397502663252 39.09868106889479,-1.9142106223355313 39.09868106889479," \
+                 "-1.9142106223355313 38.94504502508093,-2.20397502663252 38.94504502508093," \
+                 "-2.20397502663252 39.09868106889479))"
+start_time = '2017-01-16'
+end_time = '2017-01-19'
+print(data_access_component.show_stores())
+print(data_access_component.get_provided_data_types())
+emus = data_access_component.get_data_urls(BARRAX_POLYGON, start_time, end_time, 'WV_EMU,ISO_MSI_A_EMU,ISO_MSI_B_EMU')
+print(emus)
+# data_access_component.get_data_urls(BARRAX_POLYGON, start_time, end_time, 'ISO_MSI_A_EMU')
+# data_access_component.get_data_urls(BARRAX_POLYGON, start_time, end_time, 'ISO_MSI_B_EMU')
+cams = data_access_component.get_data_urls(BARRAX_POLYGON, start_time, end_time, 'CAMS')
+print(cams)
+aster_dem = data_access_component.get_data_urls(BARRAX_POLYGON, start_time, end_time, 'Aster DEM')
+print(aster_dem)
+modis = data_access_component.query(BARRAX_POLYGON, start_time, end_time, 'MCD43A1.006')
+print(modis)
+modis_files = data_access_component.get_data_urls_from_data_set_meta_infos(modis)
+print(modis_files)
+s2_data_infos = data_access_component.query(BARRAX_POLYGON, start_time, end_time, 'AWS_S2_L1C')
+print(s2_data_infos)
+s2_urls = data_access_component.get_data_urls_from_data_set_meta_infos(s2_data_infos)
+print(s2_urls)
+# uncomment these lines
+# file_refs = data_access_component.get_data_urls(BARRAX_POLYGON, '2017-04-01', '2017-04-08', 'AWS_S2_L1C')
+# print(file_refs)
