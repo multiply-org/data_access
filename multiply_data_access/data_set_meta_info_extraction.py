cdd62d610d22ceb61dfe27f532cf9e6d03526172
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-08-21 14:26:39 +0200

    added s2 l2 meta info extractor

--- multiply_data_access/data_set_meta_info_extraction.py
+++ multiply_data_access/data_set_meta_info_extraction.py
@@ -104,6 +104,77 @@ class AwsS2MetaInfoExtractor(DataSetMetaInfoExtractor):
                 return x.text
 
 
+class S2L2MetaInfoExtractor(DataSetMetaInfoExtractor):
+
+    @classmethod
+    def name(cls) -> str:
+        return DataTypeConstants.AWS_S2_L2
+
+    def extract_meta_info(self, path: str) -> DataSetMetaInfo:
+        coverage = self._extract_coverage(path)
+        time = self._extract_time_from_metadata_file(path)
+        return DataSetMetaInfo(coverage, time, time, self.name(), path)
+
+    @staticmethod
+    def _get_xml_root(xml_file_name: str):
+        tree = ElementTree.parse(xml_file_name)
+        return tree.getroot()
+
+    def _extract_time_from_metadata_file(self, filename: str) -> str:
+        """Parses the XML metadata file to extract the sensing time."""
+        root = self._get_xml_root(filename + '/metadata.xml')
+        for child in root:
+            for x in child.findall("SENSING_TIME"):
+                time = x.text.replace('T', ' ').replace('Z', '')
+                time = time[:time.rfind('.')]
+                return time
+
+    def _extract_coverage(self, filename: str) -> str:
+        """Parses the XML metadata file to extract the sensing time."""
+        root = self._get_xml_root(filename + '/metadata.xml')
+        ulx = 0
+        uly = 0
+        x_dim = 0
+        y_dim = 0
+        n_rows = 0
+        n_cols = 0
+        for child in root:
+            tile_geocoding_element = child.find('Tile_Geocoding')
+            if tile_geocoding_element is not None:
+                for element in tile_geocoding_element:
+                    if element.tag == 'Size' and element.attrib['resolution'] == '60':
+                        n_rows = float(element.find('NROWS').text)
+                        n_cols = float(element.find('NCOLS').text)
+                    elif element.tag == 'Geoposition' and element.attrib['resolution'] == '60':
+                        ulx = float(element.find('ULX').text)
+                        uly = float(element.find('ULY').text)
+                        x_dim = float(element.find('XDIM').text)
+                        y_dim = float(element.find('YDIM').text)
+        llx = ulx + n_rows * x_dim
+        lly = uly + n_cols * y_dim
+        gdal_dataset = gdal.Open(filename + '/B01_sur_unc.tif')
+        source_srs = reproject.get_spatial_reference_system_from_dataset(gdal_dataset)
+        target_srs = osr.SpatialReference()
+        target_srs.SetWellKnownGeogCS('EPSG:4326')
+        coords = [ulx, uly, llx, uly, llx, lly, ulx, lly]
+        transformed_coords = reproject.transform_coordinates(source_srs, target_srs, coords)
+        return 'POLYGON(({0} {1}, {2} {3}, {4} {5}, {6} {7}, {0} {1}))'.format(transformed_coords[0],
+                                                                               transformed_coords[1],
+                                                                               transformed_coords[2],
+                                                                               transformed_coords[3],
+                                                                               transformed_coords[4],
+                                                                               transformed_coords[5],
+                                                                               transformed_coords[6],
+                                                                               transformed_coords[7])
+
+    def _extract_tile_id(self, filename: str) -> str:
+        """Parses the XML metadata file to extract the tile id."""
+        root = self._get_xml_root(filename + '/metadata.xml')
+        for child in root:
+            for x in child.findall("TILE_ID"):
+                return x.text
+
+
 class AsterMetaInfoExtractor(DataSetMetaInfoExtractor):
 
     @classmethod
@@ -219,6 +290,7 @@ def add_data_set_meta_info_extractor(data_set_meta_info_provider: DataSetMetaInf
 
 
 add_data_set_meta_info_extractor(AwsS2MetaInfoExtractor())
+add_data_set_meta_info_extractor(S2L2MetaInfoExtractor())
 add_data_set_meta_info_extractor(AsterMetaInfoExtractor())
 add_data_set_meta_info_extractor(S2aMetaInfoExtractor())
 add_data_set_meta_info_extractor(S2bMetaInfoExtractor())
--- test/data_access/test_data_set_meta_info_extraction.py
+++ test/data_access/test_data_set_meta_info_extraction.py
@@ -1,10 +1,12 @@
-from multiply_data_access.data_set_meta_info_extraction import DataSetMetaInfoExtractor, AwsS2MetaInfoExtractor
+from multiply_data_access.data_set_meta_info_extraction import DataSetMetaInfoExtractor, AwsS2MetaInfoExtractor, \
+    S2L2MetaInfoExtractor
+
 from shapely import wkt
 
 __author__ = "Tonio Fincke (Brockmann Consult GmbH)"
 
 path_to_s2_dir = './test/test_data/aws_s2_data/29/S/QB/2017/9/4/0/'
-
+path_to_s2_l2_dir = './test/test_data/s2_l2_dir/'
 
 def test_aws_s2_meta_info_extractor():
     provider = AwsS2MetaInfoExtractor()
@@ -20,3 +22,19 @@ def test_aws_s2_meta_info_extractor():
     assert 36.906971812661624 in geometry_bounds
     assert -5.4774490849610435 in geometry_bounds
     assert 37.92559054724302 in geometry_bounds
+
+
+def test_s2_l2_meta_info_extractor():
+    provider = S2L2MetaInfoExtractor()
+    assert 'AWS_S2_L2' == provider.name()
+    data_set_meta_info = provider.extract_meta_info(path_to_s2_l2_dir)
+    assert 'AWS_S2_L2' == data_set_meta_info.data_type
+    assert path_to_s2_l2_dir == data_set_meta_info.identifier
+    assert '2017-01-19 11:05:33' == data_set_meta_info.start_time
+    assert '2017-01-19 11:05:33' == data_set_meta_info.end_time
+    geometry_bounds = wkt.loads(data_set_meta_info.coverage).bounds
+    assert 4 == len(geometry_bounds)
+    assert -3.000233454377241 in geometry_bounds
+    assert 38.754036047778804 in geometry_bounds
+    assert -1.7187196513335372 in geometry_bounds
+    assert 39.75026792656397 in geometry_bounds
